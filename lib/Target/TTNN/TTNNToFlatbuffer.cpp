7c360d1b2a94a16de65b32985b3b1375f9325027
Lewis Panos <lpanos@tenstorrent.com>
2025-10-01 16:30:25 +0000

    I forgot exactly all the things I did in this commit but like, I made page table mandatory and update verify logic

--- lib/Target/TTNN/TTNNToFlatbuffer.cpp
+++ lib/Target/TTNN/TTNNToFlatbuffer.cpp
@@ -903,7 +903,7 @@ createOp(FlatbufferObjectCache &cache, PagedUpdateCacheOp op) {
                        : 0;
   return ::tt::target::ttnn::CreatePagedUpdateCacheOp(
       *cache.fbb, cacheOperand, input, updateIndex, op.getShareCache(),
-      pageTable, op.getBatchOffset());
+      pageTable);
 }
 
 ::flatbuffers::Offset<::tt::target::ttnn::FillCacheOp>
